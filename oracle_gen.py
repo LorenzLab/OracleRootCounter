7257c88c70550f272f9ec46fd49992c7563b9843
TWTom041 <s10855041@thjh.tp.edu.tw>
2024-04-02 22:24:57 +0800

    dataset generator done

--- oracle_gen.py
+++ oracle_gen.py
@@ -1,4 +1,5 @@
 import os
+import time
 import random
 
 import numpy as np
@@ -42,7 +43,7 @@ def random_rotate(img):
     Randomly rotate image.
     """
     angle = np.random.uniform(0, 360)
-    img = ndi.rotate(img, angle, reshape=False)
+    img = ndi.rotate(img, angle, reshape=True)
     return img
 
 def random_resize(img):
@@ -64,6 +65,13 @@ def test_distort():
     plt.imshow(warped, cmap='gray')
     plt.show()
 
+def crop_to_bbox(img: np.ndarray) -> np.ndarray:
+    th = cv2.threshold(img, 20, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    coords = cv2.findNonZero(th)
+    x, y, w, h = cv2.boundingRect(coords)
+    return img[y: y+h, x: x+w]
+
+
 def load_image(path: str | int, add_noise: bool = False) -> np.ndarray:
     if isinstance(path, int):
         path = f'00_Oracle/LOBI_Roots/r{path}.bmp'
@@ -74,6 +82,7 @@ def load_image(path: str | int, add_noise: bool = False) -> np.ndarray:
         img = random_flip(img)
         img = random_rotate(img)
         img = random_resize(img)
+        img = crop_to_bbox(img)
     return img
 
 def merge_images(images: list[np.ndarray]) -> np.ndarray:
@@ -87,11 +96,11 @@ def merge_images(images: list[np.ndarray]) -> np.ndarray:
         img = images[i]
         x = np.random.randint(0, IMG_SIZE - img.shape[0])
         y = np.random.randint(0, IMG_SIZE - img.shape[1])
-        canvas[x:x+img.shape[0], y:y+img.shape[1]] = np.maximum(img, canvas[x:x+img.shape[0], y:y+img.shape[1]])
+        canvas[x: x+img.shape[0], y: y+img.shape[1]] = np.maximum(img, canvas[x: x+img.shape[0], y: y+img.shape[1]])
         bounding_boxes.append((x+img.shape[0]//2, y+img.shape[1]//2, img.shape[0], img.shape[1]))
     return canvas, bounding_boxes
 
-def get_batch_image(path: str) -> np.ndarray:
+def get_trainable_image(path: str) -> np.ndarray:
     # path is a folder containing images
     images = os.listdir(path)
     randomly_selected_images = [random.choice(range(len(images))) for _ in range(random.randint(1, 5))]
@@ -99,13 +108,36 @@ def get_batch_image(path: str) -> np.ndarray:
     out_img, bounding_boxes = merge_images(images)
     return out_img, bounding_boxes, randomly_selected_images
 
+def get_batch_image(path: str, batch_size=16) -> np.ndarray:
+    # path is a folder containing images
+    images = []
+    bounding_boxes = []
+    labels = []
+    for _ in range(batch_size):
+        out_img, bb, l = get_trainable_image(path)
+        images.append(out_img)
+        bounding_boxes.append(bb)
+        labels.append(l)
+    return images, bounding_boxes, labels
+
 def test():
-    test_distort()
+    path = "00_Oracle/LOBI_Roots"
+    with open('file_list_order.txt', 'w') as f:
+        for line in os.listdir(path):
+            f.write(f"{line}\n")
+
+    st = time.time()
+    
+    fig = plt.figure(figsize=(13, 13))
+    
+    out_img, bounding_boxes, randomly_selected_images = get_batch_image(path)
+    
+    for en, (i, j, k) in enumerate(zip(out_img, bounding_boxes, randomly_selected_images), 1):
+        plt.subplot(4, 4, en)
+        plt.axis('off')
+        plt.imshow(i, cmap='gray')
+        plt.title(f"Labels: {k}")
+    plt.show()
 
 if __name__ == '__main__':
-    path = "00_Oracle/LOBI_Roots"
-    out_img, bounding_boxes, randomly_selected_images = get_batch_image(path)
-    for i in randomly_selected_images:
-        print(os.listdir(path)[i])
-    plt.imshow(out_img, cmap='gray')
-    plt.show()
+    test()
