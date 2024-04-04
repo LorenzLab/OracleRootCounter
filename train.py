edea35118e2db5f138cd93d51675a02478989be0
TWTom041 <s10855041@thjh.tp.edu.tw>
2024-04-04 14:54:18 +0800

    train.py

---
+++ train.py
@@ -0,0 +1,23 @@
+from pt_dataset import OracleDataset
+from oracle_gen import get_trainable_image
+
+import torch
+from torch.utils.data import DataLoader
+from ultralytics.models.yolo.detect import DetectionTrainer
+
+
+class OracleDetectionTrainer(DetectionTrainer):
+    def __init__(self, dataloader, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.dataloader = dataloader
+
+
+    def get_dataloader(self, cfg, weights):
+        return self.dataloader
+
+
+def main():
+    dataset = OracleDataset(get_trainable_image, "00_Oracle/LOBI_Roots", batch_size=16, length=1000)
+    dataloader = DataLoader(dataset, batch_size=16, shuffle=False, num_workers=6)
+    trainer = OracleDetectionTrainer(dataloader)
+    trainer.train()
