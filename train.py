0f101a237be8638d40ff92f0bac0e2083f99abba
TWTom041 <s10855041@thjh.tp.edu.tw>
2024-04-04 15:23:39 +0800

    add test dataloader

--- train.py
+++ train.py
@@ -7,18 +7,28 @@ from ultralytics.models.yolo.detect import DetectionTrainer
 
 
 class OracleDetectionTrainer(DetectionTrainer):
-    def __init__(self, dataloader, *args, **kwargs):
+    def __init__(self, train_dataloader, test_dataloader, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.dataloader = dataloader
+        self.train_dataloader = train_dataloader
+        self.test_dataloader = test_dataloader
+    
+    def get_dataset(self):
+        return None, None
 
-    def get_dataloader(self, cfg, weights):
-        return self.dataloader
+    def get_dataloader(self, mode="train", *args, **kwargs):
+        if mode == "train":
+            return self.train_dataloader
+        elif mode == "test" or mode == "val":
+            return self.train_dataloader
 
 
 def main():
-    dataset = OracleDataset(get_trainable_image, "00_Oracle/LOBI_Roots", batch_size=16, length=1000)
-    dataloader = DataLoader(dataset, batch_size=16, shuffle=False, num_workers=6)
-    trainer = OracleDetectionTrainer(dataloader)
+    train_dataset = OracleDataset(get_trainable_image, "00_Oracle/LOBI_Roots", batch_size=16, length=1024)
+    train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=False, num_workers=6)
+    test_dataset = OracleDataset(get_trainable_image, "00_Oracle/LOBI_Roots", batch_size=16, length=128)
+    test_dataloader = DataLoader(test_dataset, batch_size=16, shuffle=False, num_workers=6)
+
+    trainer = OracleDetectionTrainer(train_dataloader, test_dataloader)
     trainer.train()
 
 
