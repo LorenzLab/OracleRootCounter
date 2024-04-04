4fcf2055ce34ecf0a3f51932b14bebd39df180d0
TWTom041 <s10855041@thjh.tp.edu.tw>
2024-04-04 14:57:13 +0800

    train.py

--- train.py
+++ train.py
@@ -21,3 +21,7 @@ def main():
     dataloader = DataLoader(dataset, batch_size=16, shuffle=False, num_workers=6)
     trainer = OracleDetectionTrainer(dataloader)
     trainer.train()
+
+
+if __name__ == "__main__":
+    main()
